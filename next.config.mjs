ff1d982c6d2dbf4639550087755d53488ee21194
olalere lanre <larrylere@gmail.com>
2024-06-04 20:00:41 +0100

    implement all the features

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,20 @@
 /**
  * @type {import('next').NextConfig}
  */
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
+
+
+
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     typescript:{
       ignoreBuildErrors:true
-    }
-
+    },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
    
 }
   
