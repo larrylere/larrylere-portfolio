3d7b650b449a550039549100040485af1f5513a6
olalere lanre <larrylere@gmail.com>
2024-06-04 16:08:52 +0100

    implement all the features

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+output: 'undefined',
 typescript:{
-    ignoreBuildErrors:true
+    ignoreBuildErrors:true,
 }
+};
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
